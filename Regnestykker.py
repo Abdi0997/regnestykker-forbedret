49c413a5390bf38bcf543228e611b11b592105d7
Abdi0997 <122614552+Abdi0997@users.noreply.github.com>
2023-02-22 18:53:52 +0100

    Update Regnestykker.py
    Jeg startede med at lave et trello til hvilke forbedringer jeg kunne lave, jeg valgte at arbejde med disse to: Få koden til at kunne bruge decimal tal og Få koden til at validere brugerens input
    Til at gøre det brugte jeg disse links:
    https://www.youtube.com/watch?v=u0402sx05yI&t=20s
    https://docs.python.org/3/reference/compound_stmts.html#the-while-statement (Kun til while statement)
    https://docs.python.org/3/reference/simple_stmts.html#the-break-statement (Kun til break og continue statement)

--- Regnestykker.py
+++ Regnestykker.py
@@ -6,8 +6,8 @@ def generate_question():
     # vælg en tilfældig operation
     operation = random.choice(operationer)
     # Generer to tilfældige tal mellem 1 og 10
-    num1 = random.randint(5, 10)
-    num2 = random.randint(1, 4)
+    num1 = random.randint(1, 10)
+    num2 = random.randint(1, 10)
     # Opret spørgsmålstrengen
     spørgsmål = f"Hvad er {num1} {operation} {num2}?"
     # Returnér spørgsmålet og svaret (evalueret)
@@ -15,20 +15,29 @@ def generate_question():
 
 # Variabel til at tælle antallet af korrekte svar
 rigtige_svar = 0
+
 # Spørg 10 gange
 for i in range(10):
     # Generer et spørgsmål
     spørgsmål, svar = generate_question()
     print(spørgsmål)
-    # Tag svaret fra brugeren
-    bruger_svar = input("Dit svar: ")
+    # Tag svaret fra brugeren, Replace "." med "," så der kan bruges decimaltal
+    while True:
+        bruger_svar = input("Dit svar: ").replace(",", ".")
+        try:
+            float(bruger_svar)
+        except ValueError:
+            print("Ugyldigt input, prøv igen")
+            continue
+        else:
+            break
     # Tjek om svaret er korrekt
     if float(bruger_svar) == svar:
         print("Korrekt!")
         rigtige_svar += 1
     else:
         print("Forkert!")
-    print()
+print()
 
 # Print det samlede antal rigtige svar
 print(f"Du fik {rigtige_svar} ud af 10 rigtige.")
